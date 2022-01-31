9cdb742aa30f5987fb60bc99c5de124939876797
Nazar Dzhura <nazar_dzhura@ukr.net>
2022-01-31 16:05:27 +0200

    add: request argument parser

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, request
-from flask_restful import Api, Resource
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video are required", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes on the video are required", required=True)
 
 videos = {}
 
@@ -12,8 +17,8 @@ class Video(Resource):
         return videos[video_id]
 
     def put(self, video_id):
-        print(request.form['likes'])
-        return {}
+        args = video_put_args.parse_args()
+        return {video_id: args}
 
 
 api.add_resource(Video, "/video/<int:video_id>")
