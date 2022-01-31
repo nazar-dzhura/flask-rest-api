e519aa3f1b25d0264e8c922b281e8c5689d92030
Nazar Dzhura <nazar_dzhura@ukr.net>
2022-01-31 15:54:22 +0200

    add: storing data in memory

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
-
-class HelloWorld(Resource):
-    def get(self, name, test):
-        return {"name": name, "test": test}
+videos = {}
 
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+
+    def put(self, video_id):
+        print(request.form['likes'])
+        return {}
+
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == '__main__':
     app.run(debug=True)
