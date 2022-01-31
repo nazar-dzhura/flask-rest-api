597b31cdf97c4513af41f9f19efa825c698a1b49
Nazar Dzhura <nazar_dzhura@ukr.net>
2022-01-31 16:57:33 +0200

    add: configure database and create model

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name = {name}, views = {views}, likes = {likes})"
+
+
+#db.create_all() - should be run once to create all the tables
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
@@ -16,9 +32,11 @@ def abort_if_video_id_doesnt_exist(video_id):
     if video_id not in videos:
         abort(404, message="Video id is not valid...")
 
+
 def abort_if_video_id_exists(video_id):
     if video_id in videos:
         abort(409, message="Video with that id already exists...")
+
 
 class Video(Resource):
     def get(self, video_id):
@@ -35,6 +53,7 @@ class Video(Resource):
         abort_if_video_id_doesnt_exist(video_id)
         del videos[video_id]
         return '', 204
+
 
 api.add_resource(Video, "/video/<int:video_id>")
 
