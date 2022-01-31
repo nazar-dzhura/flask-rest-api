4459b696af62b138f3c7a0345f5c82db1410fb2b
Nazar Dzhura <nazar_dzhura@ukr.net>
2022-01-31 18:10:33 +0200

    add: handling update request

--- app.py
+++ app.py
@@ -25,6 +25,12 @@ video_put_args.add_argument("name", type=str, help="Name of the video is require
 video_put_args.add_argument("views", type=int, help="Views of the video are required", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes on the video are required", required=True)
 
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str)
+video_update_args.add_argument("views", type=int)
+video_update_args.add_argument("likes", type=int)
+
+
 resource_fields = {
     'id': fields.Integer,
     'name': fields.String,
@@ -51,6 +57,24 @@ class Video(Resource):
         db.session.add(video)
         db.session.commit()
         return video, 201
+
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Video with that id doesn't exist...")
+
+        if args['name']:
+            result.name = args['name']
+        if args['views']:
+            result.views = args['views']
+        if args['likes']:
+            result.likes = args['likes']
+
+        db.session.commit()
+
+        return result
 
     def delete(self, video_id):
         abort_if_video_id_doesnt_exist(video_id)
