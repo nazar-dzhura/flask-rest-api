1f1d279c9bb177b823c6a3ac782109eed2e1bf42
Nazar Dzhura <nazar_dzhura@ukr.net>
2022-01-31 16:33:49 +0200

    add: validating requests

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"name": "Hit the road Jack", "views": 947296964, "likes": 359080})
-print(response.json())
-input()
+#response = requests.put(BASE + "video/1", {"name": "Hit the road Jack", "views": 947296964, "likes": 359080})
+#print(response.json())
+#input()
 response = requests.get(BASE + "video/1")
 print(response.json())
